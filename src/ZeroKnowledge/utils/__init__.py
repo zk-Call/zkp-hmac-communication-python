09dfd251b95a2db2fd5f16f0a1de6b2591d0d72a
zk-Call & Dev <165683529+zk-call-dev@users.noreply.github.com>
2024-04-22 10:31:16 +0300

    init

--- src/ZeroKnowledge/utils/__init__.py
+++ src/ZeroKnowledge/utils/__init__.py
@@ -5,5 +5,4 @@ from .hashing import hash_numeric, hash_data, mod, curve_by_name
 __all__ = [
     "to_bytes", "to_str", "bytes_to_int", "int_to_bytes", "data_to_b64_str", "b64d", 'dump_object', "hash_numeric", "hash_data", "mod",
     "curve_by_name"
-
 ]
